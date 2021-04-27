4b9b9e9ccccfd5a7814ef0b8e5a9bf2c63879b7e
Oskar <oskarradonkimball@gmail.com>
2021-04-27 14:15:55 -0700

    Update gallery.php

--- site/snippets/blocks/gallery.php
+++ site/snippets/blocks/gallery.php
@@ -1,10 +1,8 @@
 <?php /** @var \Kirby\Cms\Block $block */ ?>
-<figure>
-  <ul>
-    <?php foreach ($block->images()->toFiles() as $image): ?>
-    <li>
-      <?= $image ?>
-    </li>
-    <?php endforeach ?>
-  </ul>
-</figure>
+<article class="gallery" style="--columns: <?= $block->columns()->toInt() ?>">
+  <?php foreach ($block->images()->toFiles() as $image): ?>
+  <figure class="lightbox">
+    <?= $image ?>
+  </figure>
+  <?php endforeach ?>
+</article>
