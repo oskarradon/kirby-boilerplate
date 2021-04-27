ce7e72996823c29673a44930f4f3e24c1d7ef0c3
Oskar <oskarradonkimball@gmail.com>
2021-04-27 14:16:27 -0700

    Update home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,14 +1,6 @@
 <?php snippet('header', ['template' => $page->id()]); ?>
 
-<?php
-  foreach($pages->listed() as $section) {
-    if ($section->uid() == 'projects') {
-      snippet('projects', ['data' => $section]);
-    } else {
-      snippet('section', ['data' => $section]);
-    }
-  }
-?>
+<?php snippet('projects', ['data' => page('projects')]); ?>
 
 <?php snippet('footer'); ?>
 
