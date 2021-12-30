c4935f4f575230f110d6a4d8db2002cc377b5304
Oskar <oskarradonkimball@gmail.com>
2021-12-29 16:04:04 -0800

    Update projects.php

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -1,37 +1,22 @@
 <?php
 
-$projects = $data->children()->listed()->paginate(3);
-$pagination = $projects->pagination();
+$projects = $data->children()->listed();
+;
 
 ?>
-<main>
+<main id="projects">
   <!-- <h1><?= $data->title() ?></h1> -->
 
     <?php foreach ($projects as $project): ?>
       <a href="<?= $project->url() ?>">
         <figure>
-          <?= $project->image()->crop(400, 500) ?>
-          <figcaption><?= $project->title() ?></figcaption>
+          <?= $project->image()->resize(400) ?>
+          <figcaption>
+            <span><?= $project->title() ?></span>
+            <span><?= $project->category()->first() ?></span>
+          </figcaption>
         </figure>
       </a>
     <?php endforeach ?>
-
-  <?php if ($pagination->hasPages()): ?>
-  <nav id="pagination">
-    <?php if ($pagination->hasPrevPage()): ?>
-    <a href="<?= $pagination->prevPageUrl() ?>" aria-label="Go to previous page">&larr;</a>
-    <?php else: ?>
-    <span>&larr;</span>
-    <?php endif ?>
-
-    <span>Page <?= $pagination->page() ?> of <?= $pagination->pages() ?></span>
-
-    <?php if ($pagination->hasNextPage()): ?>
-    <a href="<?= $pagination->nextPageUrl() ?>" aria-label="Go to next page">&rarr;</a>
-    <?php else: ?>
-    <span>&rarr;</span>
-    <?php endif ?>
-  </nav>
-  <?php endif ?>
 
 </main>
